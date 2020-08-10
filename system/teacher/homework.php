99f0575b0d19f726d79dc95918aa211772ff9feb
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-08-10 10:31:51 -0500

    Label de Tareas

--- system/teacher/homework.php
+++ system/teacher/homework.php
@@ -2,7 +2,7 @@
 <html lang="es">
 
 <head>
-    <title>Administrador</title>
+    <title>Asignacion de Tareas</title>
     <meta charset="UTF-8">
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -14,10 +14,31 @@
     <section class="full-box dashboard-contentPage">
         <?php include "../includes/navbar.php" ?>
         <!-- A PARTIR DE AQUI CREAR CONTENIDO DE LA PAGINA-->
-        <h1>Asignación de Tareas</h1>
+
         <div class="container-fluid">
 
+            <div class="form_register">
+                <h1>Asignación de Tareas</h1>
+                <hr>
+                <div class="alert"></div>
+                <form action="">
+                    <label for="cod_nivel">Código del Nivel Educativo</label>
+                    <input type="text" name="cod_nivel" id="cod_nivel" placeholder="Nivel Educativo">
+                    <label for="cod_asignatura">Código de Asignatura</label>
+                    <input type="text" name="cod_asignatura" id="cod_asignatura" placeholder="Codigo de Asignatura">
+                    <label for="cod_periodo">Código del Periodo Electivo</label>
+                    <input type="text" name="cod_periodo" id="cod_periodo" placeholder="Periodo Electivo">
+                    <label for="cod_paralelo">Código del Paralelo</label>
+                    <input type="text" name="cod_paralelo" id="cod_paralelo" placeholder="Código de Paralelo">
+                    <label for="cod_docente">Código de Docente</label>
+                    <input type="text" name="cod_docente" id="cod_docente" placeholder="Código Docento">
+                    <label for="cod_tarea">Descripcion de Tarea</label>
+                    <input type="text" name="cod_tarea" id="cod_tarea" placeholder="Descripcion de Tarea">
 
+                    <input type="submit" value="Registrar Tarea" class="btn_save">
+                    
+                </form>
+            </div>
 
         </div>
     </section>
