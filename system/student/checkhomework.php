4db2a64e1ce84569c290b12788d7c5522c55d70a
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-08-10 18:01:20 -0500

    Estudiante

--- system/student/checkhomework.php
+++ system/student/checkhomework.php
@@ -2,9 +2,10 @@
 <html lang="es">
 
 <head>
-    <title>Estudiante</title>
+    <title>Tareas Pendientes</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="../css/main.css">
 </head>
 
@@ -14,7 +15,9 @@
         <?php include "../includes/navbar.php" ?>
         <!-- A PARTIR DE AQUI CREAR CONTENIDO DE LA PAGINA-->
         <h1>Estudiante</h1>
-        <div class="container-fluid"><!-- TODO LO QUE CREEN DEBE IR SEPARADO DENTRO DE ESTE CONTENEDOR-->
+        <div class="container-fluid">
+            <!-- TODO LO QUE CREEN DEBE IR SEPARADO DENTRO DE ESTE CONTENEDOR-->
+
         </div>
     </section>
 </body>
