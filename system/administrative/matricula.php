49cc9624a95227a93b11c69d1cb87f5190ae4048
Erick Montero <atlhasdemon@hotmail.com>
2020-08-10 17:41:09 -0500

    Update matricula.php

--- system/administrative/matricula.php
+++ system/administrative/matricula.php
@@ -15,7 +15,16 @@
         <!-- A PARTIR DE AQUI CREAR CONTENIDO DE LA PAGINA-->
         <h1>Personal Administrativo</h1>
         <div class="container-fluid"><!-- TODO LO QUE CREEN DEBE IR SEPARADO DENTRO DE ESTE CONTENEDOR-->
-
+        <div class="container">
+                <!-- Featured Project Row-->
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h3>Matricula automatica</h3>
+                            <h4>
+                            <?php ?>
+                        </h4>
+                        </div>
+        </div>
         </div>
     </section>
 </body>
