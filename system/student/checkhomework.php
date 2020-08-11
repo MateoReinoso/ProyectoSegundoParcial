a5474296590ad46b81cabbaba4a8ed1c55dbf581
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-08-10 21:09:29 -0500

    Estudiante consulta materia

--- system/student/checkhomework.php
+++ system/student/checkhomework.php
@@ -22,12 +22,16 @@
                     <h1>Tareas por Realizar</h1>
                     <hr>
                     <form action="" method="post">
-                        <label for="rol">Materia</label>
-                        <input type="text" name="rol" id="rol">
+                        <label for="">Materia</label>
+                        <input type="text" name="rol">
+                        <label for="">Codigo Materia</label>
+                        <input type="text" name="cod">
                         <input type="submit" value="Consultar">
                        
                     </form>
-                    
+                    <?php 
+                        $varible = $_POST['rol'];
+                    ?>
                     <hr>
                     <table>
                         <tr>
@@ -37,11 +41,10 @@
                         </tr>
                         
                         <?php
-                        $valor = 5;
-                        $mate = 'Lenguaje';
+                        $valor = '5';
                                 $query = mysqli_query($connection, "SELECT  a.COD_ASIGNATURA, a.NOMBRE, ta.DETALLE_TAREA 
                                 FROM tarea_asignatura ta, alumno_asignatura_periodo aap, usuario u, asignatura a
-                                WHERE a.NOMBRE = '$mate'
+                                WHERE a.NOMBRE = '$varible'
                                 AND aap.COD_ALUMNO = '$valor'
                                 AND u.COD_PERSONA = '$valor'");
 
