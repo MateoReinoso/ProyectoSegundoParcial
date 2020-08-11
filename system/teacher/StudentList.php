6c214697169abfa7364220a37dc4ae952aa5173e
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-08-11 18:22:09 -0500

    final

--- system/teacher/StudentList.php
+++ system/teacher/StudentList.php
@@ -7,7 +7,8 @@ include '../sql_connection/conexion.php';
 <head>
     <title>Lista de Estudiantes</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="../css/main.css">
 </head>
 
@@ -28,10 +29,13 @@ include '../sql_connection/conexion.php';
 
                 <?php
                     $valor = 1920;
+            
+                   
                     $query = mysqli_query($connection, "SELECT aap.COD_PERIODO_LECTIVO, ap.COD_AULA, p.APELLIDO, p.NOMBRE FROM alumno_asignatura_periodo aap, asignatura_periodo ap, persona p
                     WHERE ap.COD_DOCENTE = aap.COD_DOCENTE
                     AND p.COD_PERSONA = aap.COD_ALUMNO
-                    AND aap.COD_PERIODO_LECTIVO = '$valor'");
+                    AND aap.COD_PERIODO_LECTIVO = '$valor'
+                    ORDER BY APELLIDO ASC");
 
                     $result = mysqli_num_rows($query);
 
@@ -39,18 +43,18 @@ include '../sql_connection/conexion.php';
                         
                         while ($data = mysqli_fetch_array($query)) {
                         ?>
-                            <tr>
-                                <td> <?php echo $data["COD_PERIODO_LECTIVO"] ?> </td>
-                                <td> <?php echo $data["COD_AULA"] ?> </td>
-                                <td> <?php echo $data["APELLIDO"] ?> </td>
-                                <td> <?php echo $data["NOMBRE"] ?> </td>
-                            </tr>
-                        <?php
+                <tr>
+                    <td> <?php echo $data["COD_PERIODO_LECTIVO"] ?> </td>
+                    <td> <?php echo $data["COD_AULA"] ?> </td>
+                    <td> <?php echo $data["APELLIDO"] ?> </td>
+                    <td> <?php echo $data["NOMBRE"] ?> </td>
+                </tr>
+                <?php
                         }
                     }
                 ?>
 
-                
+
             </table>
         </div>
     </section>
